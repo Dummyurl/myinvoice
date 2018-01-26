d698e3e66d1f8df8bf2a38dded7fbf9a65c3aee8
user <user@MULTI-02>
2018-01-26 12:11:00 +0530

    Added by Prashant

---
+++ app/modules/root/views/home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Bootstrap Example</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="<?php echo base_url('home') ?>"><?php echo MY_SITE_NAME ?></a>
+                </div>
+                
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="<?php echo base_url('register') ?>"><span class="glyphicon glyphicon-user"></span> Register</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url('login') ?>"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+    </body>
+</html>
