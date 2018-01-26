e2d9ece6cb6822162812214c8e8cfa17e25c971c
user <user@MULTI-01>
2018-01-26 14:03:39 +0530

    nikul

--- app/config/routes.php
+++ app/config/routes.php
@@ -16,6 +16,7 @@ $route['logout'] = 'root/logout';
 $route['forgotpassword'] = 'root/forgotpassword';
 
 $route['reset_password_user'] = 'root/reset_password_user';
+$route['dashboard'] = 'content/dashboard';
 /*
  *  Frontend Routes
  */
@@ -23,17 +24,17 @@ $route['reset_password_user'] = 'root/reset_password_user';
 //$route['login'] = 'content/login';
 //$route['logout'] = 'content/logout';
 //$route['reset_password'] = 'content/reset_password';
-//$route['profile'] = 'content/profile';
-//$route['dashboard'] = 'content/dashboard';
+$route['profile'] = 'content/profile';
+$route['dashboard'] = 'content/dashboard';
 //$route['forgotpassword'] = 'content/forgotpassword';
 //$route['reset_password_user'] = 'content/reset_password_user';
 //$route['reset_password_admin'] = 'content/reset_password_admin';
 //$route['reset_password_partner'] = 'content/reset_password_partner';
-//$route['content_management'] = 'content/content_management';
-//$route['add_content'] = 'content/add_content';
-//$route['update_content'] = 'content/update_content';
-//$route['content_update_action'] = 'content/content_update_action';
-//$route['content_add_action'] = 'content/content_add_action';
+$route['content_management'] = 'content/content_management';
+$route['add_content'] = 'content/add_content';
+$route['update_content'] = 'content/update_content';
+$route['content_update_action'] = 'content/content_update_action';
+$route['content_add_action'] = 'content/content_add_action';
 
 //User
 $route['user_details'] = 'users/userDetails';
